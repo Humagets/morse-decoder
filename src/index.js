601b0f522ea1746804ebcb6b6a1abccb98014bed
Ruslan Berezko <68183783+Humagets@users.noreply.github.com>
2023-02-03 19:04:34 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,54 +1,57 @@
-function decode(expr) {
-  const morseCodeDict = {
-    'a': '.-',
-    'b': '-...',
-    'c': '-.-.',
-    'd': '-..',
-    'e': '.',
-    'f': '..-.',
-    'g': '--.',
-    'h': '....',
-    'i': '..',
-    'j': '.---',
-    'k': '-.-',
-    'l': '.-..',
-    'm': '--',
-    'n': '-.',
-    'o': '---',
-    'p': '.--.',
-    'q': '--.-',
-    'r': '.-.',
-    's': '...',
-    't': '-',
-    'u': '..-',
-    'v': '...-',
-    'w': '.--',
-    'x': '-..-',
-    'y': '-.--',
-    'z': '--..',
-    '1': '.----',
-    '2': '..---',
-    '3': '...--',
-    '4': '....-',
-    '5': '.....',
-    '6': '-....',
-    '7': '--...',
-    '8': '---..',
-    '9': '----.',
-    '0': '-----',
-  };
+const MORSE_TABLE = {
+    '.-':     'a',
+    '-...':   'b',
+    '-.-.':   'c',
+    '-..':    'd',
+    '.':      'e',
+    '..-.':   'f',
+    '--.':    'g',
+    '....':   'h',
+    '..':     'i',
+    '.---':   'j',
+    '-.-':    'k',
+    '.-..':   'l',
+    '--':     'm',
+    '-.':     'n',
+    '---':    'o',
+    '.--.':   'p',
+    '--.-':   'q',
+    '.-.':    'r',
+    '...':    's',
+    '-':      't',
+    '..-':    'u',
+    '...-':   'v',
+    '.--':    'w',
+    '-..-':   'x',
+    '-.--':   'y',
+    '--..':   'z',
+    '.----':  '1',
+    '..---':  '2',
+    '...--':  '3',
+    '....-':  '4',
+    '.....':  '5',
+    '-....':  '6',
+    '--...':  '7',
+    '---..':  '8',
+    '----.':  '9',
+    '-----':  '0',
+};
 
-  const words = expr.split('   ');
-  const decodedWords = words.map((word) => {
-    const letters = word.split(' ');
-    const decodedLetters = letters.map((letter) => {
-      for (const key in morseCodeDict) {
-        if (morseCodeDict[key] === letter) {
-          return key;
+function decode(expr) {
+    let words = expr.split("**********");
+    let decoded = "";
+    for (let i = 0; i < words.length; i++) {
+        let letters = words[i].match(/.{10}/g);
+        for (let j = 0; j < letters.length; j++) {
+            let code = "";
+            for (let k = 0; k < letters[j].length; k++) {
+                code += letters[j][k] === "0" ? "." : "-";
+            }
+            decoded += MORSE_TABLE[code];
         }
-      }
-    });
-    return decodedLetters.join('');
-  });
-  return decodedWords.join(' ');
+        decoded += " ";
+    }
+    return decoded.trim();
 }
+
+console.log(decode("00000011110000000010**********0000001111")); // outputs "me"
