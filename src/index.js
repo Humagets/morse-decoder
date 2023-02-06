d72bb86f8d073b2b0066b2c25ce08a92a5624fc4
Ruslan Berezko <68183783+Humagets@users.noreply.github.com>
2023-02-06 11:40:11 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,56 +1,56 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
-};
-
-function decode(expr) {
-    const morseCodeArray = expr.split('**********');
-    let decodedMessage = '';
-    for (const morseCode of morseCodeArray) {
-        const letter = MORSE_TABLE[morseCode.replace(/0/g, '')];
-        if (letter) {
-            decodedMessage += letter;
-        } else {
-            decodedMessage += ' ';
-        }
+    '.-': 'a',
+    '-...': 'b',
+    '-.-.': 'c',
+    '-..': 'd',
+    '.': 'e',
+    '..-.': 'f',
+    '--.': 'g',
+    '....': 'h',
+    '..': 'i',
+    '.---': 'j',
+    '-.-': 'k',
+    '.-..': 'l',
+    '--': 'm',
+    '-.': 'n',
+    '---': 'o',
+    '.--.': 'p',
+    '--.-': 'q',
+    '.-.': 'r',
+    '...': 's',
+    '-': 't',
+    '..-': 'u',
+    '...-': 'v',
+    '.--': 'w',
+    '-..-': 'x',
+    '-.--': 'y',
+    '--..': 'z',
+    '.----': '1',
+    '..---': '2',
+    '...--': '3',
+    '....-': '4',
+    '.....': '5',
+    '-....': '6',
+    '--...': '7',
+    '---..': '8',
+    '----.': '9',
+    '-----': '0',
+    };
+    
+    function decode(expr) {
+    let result = "";
+    let exprArray = expr.split("**********");
+    for (let i = 0; i < exprArray.length; i++) {
+    let current = exprArray[i];
+    let morseCode = "";
+    for (let j = 0; j < current.length; j+=2) {
+    morseCode += current[j] === "0" ? "." : "-";
     }
-    return decodedMessage;
-}
-
-module.exports = {
+    result += MORSE_TABLE[morseCode] ? MORSE_TABLE[morseCode] : " ";
+    }
+    return result;
+    }
+    
+    module.exports = {
     decode
-}
+    }
