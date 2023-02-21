753570e1d28716da6603970bfc44de22dc575f63
Ruslan Berezko <68183783+Humagets@users.noreply.github.com>
2023-02-21 09:47:33 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,61 +1,39 @@
-const MORSE_TABLE = {
-'.-': 'a',
-'-...': 'b',
-'-.-.': 'c',
-'-..': 'd',
-'.': 'e',
-'..-.': 'f',
-'--.': 'g',
-'....': 'h',
-'..': 'i',
-'.---': 'j',
-'-.-': 'k',
-'.-..': 'l',
-'--': 'm',
-'-.': 'n',
-'---': 'o',
-'.--.': 'p',
-'--.-': 'q',
-'.-.': 'r',
-'...': 's',
-'-': 't',
-'..-': 'u',
-'...-': 'v',
-'.--': 'w',
-'-..-': 'x',
-'-.--': 'y',
-'--..': 'z',
-'.----': '1',
-'..---': '2',
-'...--': '3',
-'....-': '4',
-'.....': '5',
-'-....': '6',
-'--...': '7',
-'---..': '8',
-'----.': '9',
-'-----': '0',
-};
+function decodeMorse(morseCode) {
+  const morseAlphabet = {
+    '0000001011': 'A',
+    '0011101010': 'B',
+    '0011101110': 'C',
+    '0000111010': 'D',
+    '0000000010': 'E',
+    '0010101110': 'F',
+    '0000111110': 'G',
+    '0010101010': 'H',
+    '0000001010': 'I',
+    '0010111111': 'J',
+    '0000111011': 'K',
+    '0010111010': 'L',
+    '0000001111': 'M',
+    '0000001110': 'N',
+    '0000111111': 'O',
+    '0010111110': 'P',
+    '0011110111': 'Q',
+    '0000101110': 'R',
+    '0000101010': 'S',
+    '0000000011': 'T',
+    '0000101011': 'U',
+    '0010101011': 'V',
+    '0000101111': 'W',
+    '0011101011': 'X',
+    '0011101111': 'Y',
+    '0011111010': 'Z'
+  };
+  
+  const words = morseCode.split('**********');
+  const decodedWords = words.map((word) => {
+    const letters = word.match(/.{10}/g);
+    const decodedLetters = letters.map((letter) => morseAlphabet[letter]);
+    return decodedLetters.join('');
+  });
 
-function decode(expr) {
-const words = expr.split("**********");
-let decoded = "";
-for (const word of words) {
-let letters = word.match(/.{10}/g);
-for (const letter of letters) {
-let decodedLetter = "";
-if (letter === "0000000000") {
-decodedLetter = " ";
-} else {
-decodedLetter = MORSE_TABLE[letter.replace(/0/g, ".").replace(/1/g, "-")];
+  return decodedWords.join(' ');
 }
-decoded += decodedLetter;
-}
-decoded += " ";
-}
-return decoded.trim();
-}
-
-export default {
-decode
-};
